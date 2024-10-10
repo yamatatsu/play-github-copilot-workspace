752ec0566494addbfe2c9b5243a570de6d8a83fa
yamatatsu <the.kinnikuman@gmail.com>
2024-10-10 23:04:07 +0900

    remove js files

--- src/App.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-var App = function () {
-    return ((0, jsx_runtime_1.jsx)("div", { children: (0, jsx_runtime_1.jsx)("h1", { children: "Hello, React with Vite!" }) }));
-};
-exports.default = App;
