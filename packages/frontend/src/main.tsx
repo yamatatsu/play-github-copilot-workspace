4df0a3fdc5a6af2504e716f1c8560c64e884aa52
yamatatsu <the.kinnikuman@gmail.com>
2024-10-20 11:50:53 +0900

    setup amplify-ui

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,8 +1,19 @@
+import "@aws-amplify/ui-react/styles.css";
 import "@cloudscape-design/global-styles/index.css";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { Amplify } from "aws-amplify";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
+
+Amplify.configure({
+	Auth: {
+		Cognito: {
+			userPoolId: import.meta.env.VITE_USER_POOL_ID,
+			userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+		},
+	},
+});
 
 const router = createRouter({ routeTree });
 
--- packages/frontend/src/routes/__root.tsx
+++ packages/frontend/src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { Authenticator } from "@aws-amplify/ui-react";
 import AppLayout from "@cloudscape-design/components/app-layout";
 import SideNavigation from "@cloudscape-design/components/side-navigation";
 import TopNavigation from "@cloudscape-design/components/top-navigation";
@@ -14,41 +15,51 @@ export const Route = createRootRoute({
 		const { pathname } = useLocation();
 
 		return (
-			<>
-				<TopNavigation
-					identity={{
-						href: "#",
-						title: "Service Name",
-						logo: {
-							src: "https://via.placeholder.com/30",
-							alt: "Service Logo",
-						},
-					}}
-					utilities={[
-						{
-							type: "button",
-							text: "Button",
-							href: "#",
-						},
-					]}
-				/>
-				<AppLayout
-					content={<Outlet />}
-					navigation={
-						<SideNavigation
-							activeHref={pathname}
-							onFollow={(event) => {
-								event.preventDefault();
-								navigate({ to: event.detail.href });
+			<Authenticator hideSignUp>
+				{({ user, signOut }) => (
+					<>
+						<TopNavigation
+							identity={{
+								href: "#",
+								title: "Service Name",
+								logo: {
+									src: "https://via.placeholder.com/30",
+									alt: "Service Logo",
+								},
 							}}
-							items={[
-								{ type: "link", text: "Home", href: "/home" },
-								{ type: "link", text: "About", href: "/about" },
+							utilities={[
+								{
+									type: "menu-dropdown",
+									text: user?.signInDetails?.loginId,
+									iconName: "user-profile",
+									onItemClick: (event) => {
+										if (event.detail.id === "signOut") {
+											signOut?.();
+										}
+									},
+									items: [{ id: "signOut", text: "Sign out" }],
+								},
 							]}
 						/>
-					}
-				/>
-			</>
+						<AppLayout
+							content={<Outlet />}
+							navigation={
+								<SideNavigation
+									activeHref={pathname}
+									onFollow={(event) => {
+										event.preventDefault();
+										navigate({ to: event.detail.href });
+									}}
+									items={[
+										{ type: "link", text: "Home", href: "/home" },
+										{ type: "link", text: "About", href: "/about" },
+									]}
+								/>
+							}
+						/>
+					</>
+				)}
+			</Authenticator>
 		);
 	},
 });
