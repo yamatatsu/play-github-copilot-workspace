b985c254827b22a90e4a38a30417d64432bd7dc6
yamatatsu <the.kinnikuman@gmail.com>
2024-10-25 17:00:40 +0900

    chore: remove unused env

--- packages/backend/src/utils/env.ts
+++ packages/backend/src/utils/env.ts
@@ -1,8 +1,5 @@
-const { NODE_ENV, JWT_ISSUER, JWT_AUDIENCE } = process.env;
+const { JWT_ISSUER, JWT_AUDIENCE } = process.env;
 
-if (!NODE_ENV) {
-	throw new Error("NODE_ENV is required");
-}
 if (!JWT_ISSUER) {
 	throw new Error("JWT_ISSUER is required");
 }
@@ -10,4 +7,4 @@ if (!JWT_AUDIENCE) {
 	throw new Error("JWT_AUDIENCE is required");
 }
 
-export default { NODE_ENV, JWT_ISSUER, JWT_AUDIENCE };
+export default { JWT_ISSUER, JWT_AUDIENCE };
