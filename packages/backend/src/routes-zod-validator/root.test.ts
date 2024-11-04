8487bb283f280a106f646f4443379dc6b5afe906
yamatatsu <the.kinnikuman@gmail.com>
2024-11-04 10:31:11 +0900

    use @hono/zod-openapi

--- packages/backend/src/routes-zod-validator/root.test.ts
+++
@@ -1,8 +0,0 @@
-import { testClient } from "hono/testing";
-import route from "./root";
-
-test("test", async () => {
-	const res = await testClient(route).index.$get();
-
-	expect(await res.json()).toEqual({ message: "Hello Node.js!" });
-});
--- packages/backend/src/routes-zod-validator/tasks/list.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from "@/db";
-import { Hono } from "hono";
-
-export default new Hono().get("/tasks", async (c) => {
-	const tasks = await prisma.task.findMany();
-	return c.json(tasks, 200);
-});
